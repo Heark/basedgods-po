829b38252c26167941b93b4a648d143ae5ee759e
Heark <hearkofsorcerey@gmail.com>
2015-07-18 23:16:23 -0400

    Update bgscripts.js

--- bgscripts.js
+++ bgscripts.js
@@ -6,13 +6,10 @@ var steam = '{"steamroller":[' +
 '{"Name":"JoyFrost","Score":0 },' +
 '{"Name":"Heark","Score":1 }]}';
 
-var steamobj = JSON.parse(steam).steamroller;
 var jsonText = '{"data":{"one":21,"two":35,"three":24,"four":2,"five":18},"meta":{"title":"Happy with the service"}}'
 var data = JSON.parse(jsonText).data
 var maxProp = null
 var maxValue = -1
-
-
 Config = {
     // Configuration for the script.
     servername: "Based Gods",
@@ -234,28 +231,46 @@ poScript = ({
 
         stepCounter++;
      if(stepCounter % 1 === 0){
-         var date = new Date;
-          var seconds = date.getSeconds();
-		    var minutes = date.getMinutes();
-			 var hour = date.getHours();
-          var server_time = hour+":"+minutes+":"+seconds;
+	   var steam_name = sys.getFileContent("plugins/name.txt")
+       var steam_score = sys.getFileContent("plugins/score.txt")
+	   if(steam_name == undefined){
+	      steam_name = "None"
+		  steam_score = "None"
+		}  
+         var d = new Date;
+var hour = d.getHours() == 0 ? 12 : (d.getHours() > 12 ? d.getHours() - 12 : d.getHours());
+var min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+var ampm = d.getHours() < 12 ? 'AM' : 'PM';
+          var server_time =hour + ':' + min + ' ' + ampm;
        
-          sys.setAnnouncement('<b><table width=\'100%\' border-color: #1871d4;\'><body style=\'background-color: qlineargradient(y1:0,x1:0,x2:1,y2:0 stop:0 orange,stop:.1 white,stop:.5 orange, stop:.9 white,stop:1 orange)\'><tr><td><br> <br><img src=\'pokemon:num=91&shiny=false\'></td><td><img src=\'pokemon:num=383-1&shiny=false\' align=left><img src=\'pokemon:num=717&shiny=false\' align=right><center><font face=\'Ketchum\' font size=\'12\'>Based Gods</font></center><hr><center>Links: <a href=\'http://fullmetal.boards.net/thread/51/based-gods-league-originally-oora\'>League Info</a> | </font><a href=\'https://pokemonshowdown.com/damagecalc/\'> Damage Calculator</a> | <a href=\'http://fullmetal.boards.net/\'>Forums</a> | <a href=\'http://fullmetal.boards.net/board/9/rate-team\'>Rate my teams</a></center><hr><center><font size=\'7\'></font></center><center>Server Time: '+server_time+'<br><td><br><img src=\'pokemon:491\' align=right></td></tr></table> </b>')
+          sys.setAnnouncement('<b><table width=\'100%\' border-color: #1871d4;\'><body style=\'background-color: qlineargradient(y1:0,x1:0,x2:1,y2:0 stop:0 orange,stop:.1 white,stop:.5 orange, stop:.9 white,stop:1 orange)\'><tr><td><br> <br><img src=\'pokemon:num=91&shiny=false\'></td><td><img src=\'pokemon:num=383-1&shiny=false\' align=left><img src=\'pokemon:num=717&shiny=false\' align=right><center><font face=\'Ketchum\' font size=\'12\'>Based Gods</font></center><hr><center>Links: <a href=\'http://fullmetal.boards.net/thread/51/based-gods-league-originally-oora\'>League Info</a> | </font><a href=\'https://pokemonshowdown.com/damagecalc/\'> Damage Calculator</a> | <a href=\'http://fullmetal.boards.net/\'>Forums</a> | <a href=\'http://fullmetal.boards.net/board/9/rate-team\'>Rate my teams</a></center><hr><center><font size=\'7\'></font></center><center>Server Time: '+server_time+'<br><center>Current Steamroller: '+steam_name+'<center> Score: '+steam_score+'</center></center><td><br><img src=\'pokemon:491\' align=right></td></tr></table> </b>')
 
         }
          if (stepCounter % 900 === 0){
-				var users = ["±Flippy", "±Random Guy", "±Glorious Prostitute", "±Elderly Pedophile"]
+				var users = ["±Flippy", "±Random Guy", "±Glorious Prostitute", "±Elderly Pedophile", "±Omar", "±Harry Pothead", "±Infamous Sex Addict", "±Friendly Drug Dealer", "±Attractive Carsalesman", "±Drunken Nana", "±Pregnant Suicidal Teen", "±Fleece Johnson", "±Wrong Caller ID", "Ebola Stricken Cowboy"]
              var random_name = users[Math.floor(Math.random() * users.length)];
-             var messages = ["I'll slap you so hard, even google won't be able to find you.", "It's your birthday? Well merry thanksgiving easter bunny.", "All my life I thought air was free, until I bought a bag of crisps.", "Hey I'll be back in 5 minutes, but if i'm not, just read this message again."]
+             var messages = ["I'll slap you so hard, even google won't be able to find you.", "It's your birthday? Well merry thanksgiving easter bunny.", "All my life I thought air was free, until I bought a bag of crisps.", "Hey I'll be back in 5 minutes, but if i'm not, just read this message again.", "Deja Poo, that feeling that you've heard this shit before.", "You call me fat!? Oh hell no, hold my cake.", "When I die, I want my last words to be: I left a million dollars under the...", "The worst thing about eating an entire block of cheese by yourself... is what I just said.", "Roads? Where we're going we don't need roads.", "What does a nosey pepper do?  Get jalapeno business!", "What do you call a fake noodle? An impasta."]
 				var random_message = messages[Math.floor(Math.random() * messages.length)];
-            var col1 = +Math.floor(Math.random() * 9) + 1
+            var waitress_message = "Would you like a table?"
+            var me_name = "±Based Insider"
+            var me_message = "No not at all, I came here to sit on the floor. Carpet for 5 please."
+            if(random_name == "±Omar"){
+               random_message = "I'm coming."
+             }
+             var col1 = +Math.floor(Math.random() * 9) + 1
 				var col2 = +Math.floor(Math.random() * 9) + 1
 				var col3 = +Math.floor(Math.random() * 9) + 1
 				var col4 = +Math.floor(Math.random() * 9) + 1
 				var col5 = +Math.floor(Math.random() * 9) + 1
 				var col6 = +Math.floor(Math.random() * 9) + 1
-            var random_colour = "#"+col1+""+col2+""+col3+""+col4+""+col5+""+col6;  
+            var random_colour = "#"+col1+""+col2+""+col3+""+col4+""+col5+""+col6;
+            if(random_name == "±Below Average IQ Waitress"){
+            	sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + random_name + ":</b> " + waitress_message +"</i></font>", 0);
+             sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + me_name + ":</b> " + me_message +"</i></font>", 0);
+
+            } else {
 				sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + random_name + ":</b> " + random_message +"</i></font>", 0);
+           }
 		  }
         require.callPlugins("step");
     },
