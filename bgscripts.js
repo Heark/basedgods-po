8c03e47a04840ce6dc8456e4edea1e765a29275a
Heark <hearkofsorcerey@gmail.com>
2015-07-19 20:09:25 -0400

    Update bgscripts.js

--- bgscripts.js
+++ bgscripts.js
@@ -3,8 +3,8 @@
     Credit to: Max, Lutra
 */
 var steam = '{"steamroller":[' +
-'{"Name":"JoyFrost","Score":0 },' +
-'{"Name":"Heark","Score":1 }]}';
+    '{"Name":"JoyFrost","Score":0 },' +
+    '{"Name":"Heark","Score":1 }]}';
 
 var jsonText = '{"data":{"one":21,"two":35,"three":24,"four":2,"five":18},"meta":{"title":"Happy with the service"}}'
 var data = JSON.parse(jsonText).data
@@ -15,7 +15,7 @@ Config = {
     servername: "Based Gods",
 
     maintainers: ['Heark'],
-    
+
     // Repo to load plugins from.
     repourl: "http://meteor-falls.github.io/Scripts/",
     // Repo to load data (announcement/description + tiers) from.
@@ -67,14 +67,21 @@ Config = {
         }
 
         if (!sys.fileExists(__fname)) {
-            throw {name: "NoFileError", toString: function () { return "Couldn't find file " + (__fname) + "."; }};
+            throw {
+                name: "NoFileError",
+                toString: function() {
+                    return "Couldn't find file " + (__fname) + ".";
+                }
+            };
         }
 
         __fileContent = sys.getFileContent(__fname);
 
         module = {
             exports: {},
-            reload: function () { return false; },
+            reload: function() {
+                return false;
+            },
             name: name
         };
 
@@ -106,7 +113,7 @@ Config = {
 
     if (!require.cache || (typeof FULLRELOAD === 'boolean' && FULLRELOAD === true)) {
         require.cache = {};
-        require.meta  = {};
+        require.meta = {};
     }
 
     FULLRELOAD = false;
@@ -162,23 +169,26 @@ function poUser(id) {
 
     // This is an array so we can track multiple emotes in their last message.
     this.lastEmote = [];
-    this.lastMessage = {message: "", time: 0};
+    this.lastMessage = {
+        message: "",
+        time: 0
+    };
 }
 
 function poChannel(chanId) {
-    this.id   = chanId;
+    this.id = chanId;
     this.name = sys.channel(chanId);
 
     this.creator = '';
-    this.topic   = '';
-    this.setBy   = '';
+    this.topic = '';
+    this.setBy = '';
 
     this.members = {};
-    this.auth    = {};
-    this.mutes   = {};
-    this.bans    = {};
+    this.auth = {};
+    this.mutes = {};
+    this.bans = {};
 
-    this.bots     = true;
+    this.bots = true;
     this.isPublic = true;
 }
 
@@ -224,64 +234,64 @@ poScript = ({
 
         sys.resetProfiling();
     },
-    step: function () {
+    step: function() {
         if (typeof stepCounter === "undefined") {
             stepCounter = 0;
         }
 
         stepCounter++;
-     if(stepCounter % 1 === 0){
-     	var steam_name;
-     	var steam_score;
-     	if(sys.exists("plugins/name.txt") == true){
-	   steam_name = sys.getFileContent("plugins/name.txt")
-       steam_score = sys.getFileContent("plugins/score.txt")
-		if(steam_name == undefined || " "){
-			steam_score = "None"
-			steam_name = "None"
-			} else {
-           steam_score = sys.getFileContent("plugins/score.txt")
-		   steam_name = sys.getFileContent("plugins/name.txt")
-		   }
-     	} else {
-     		// Do nothing
-     	}
-     	
-         var d = new Date;
-var hour = d.getHours() == 0 ? 12 : (d.getHours() > 12 ? d.getHours() - 12 : d.getHours());
-var min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-var ampm = d.getHours() < 12 ? 'AM' : 'PM';
-          var server_time =hour + ':' + min + ' ' + ampm;
-       
-          sys.setAnnouncement('<b><table width=100% border-color: \'#fffff\';\'><body style=\'background-color: qlineargradient(y1:0,x1:0,x2:1,y2:0 stop:0 orange,stop:.1 white,stop:.5 orange, stop:.9 white,stop:1 orange)\'><tr><td><br> <br><img src=\'pokemon:num=91&shiny=false\'></td><td><img src=\'pokemon:num=383-1&shiny=false\' align=left><img src=\'pokemon:num=717&shiny=false\' align=right><center><font face=\'Ketchum\' font size=\'12\'>Based Gods</font></center><hr><center>Links: <a href=\'http://fullmetal.boards.net/thread/51/based-gods-league-originally-oora\'>League Info</a> | </font><a href=\'https://pokemonshowdown.com/damagecalc/\'> Damage Calculator</a> | <a href=\'http://fullmetal.boards.net/\'>Forums</a> | <a href=\'http://fullmetal.boards.net/board/9/rate-team\'>Rate my teams</a></center><hr><center><font size=\'7\'></font></center><center>Server Time: '+server_time+'<br><a href="http://www.basedgods.co.nr">Showdown Server!</a><center>Current Steamroller: '+steam_name+'<center> Score: '+steam_score+'</center></center><td><br><img src=\'pokemon:491\' align=right></td></tr></table> </b>')
+        if (stepCounter % 1 === 0) {
+            var steam_name;
+            var steam_score;
+            if (sys.exists("plugins/name.txt") == true) {
+                steam_name = sys.getFileContent("plugins/name.txt")
+                steam_score = sys.getFileContent("plugins/score.txt")
+                if (steam_name == undefined || " ") {
+                    steam_score = "None"
+                    steam_name = "None"
+                } else {
+                    steam_score = sys.getFileContent("plugins/score.txt")
+                    steam_name = sys.getFileContent("plugins/name.txt")
+                }
+            } else {
+                // Do nothing
+            }
+
+            var d = new Date;
+            var hour = d.getHours() == 0 ? 12 : (d.getHours() > 12 ? d.getHours() - 12 : d.getHours());
+            var min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+            var ampm = d.getHours() < 12 ? 'AM' : 'PM';
+            var server_time = hour + ':' + min + ' ' + ampm;
+
+            sys.setAnnouncement('<b><table width=100% border-color: \'#fffff\';\'><body style=\'background-color: qlineargradient(y1:0,x1:0,x2:1,y2:0 stop:0 orange,stop:.1 white,stop:.5 orange, stop:.9 white,stop:1 orange)\'><tr><td><br> <br><img src=\'pokemon:num=91&shiny=false\'></td><td><img src=\'pokemon:num=383-1&shiny=false\' align=left><img src=\'pokemon:num=717&shiny=false\' align=right><center><font face=\'Ketchum\' font size=\'12\'>Based Gods</font></center><hr><center>Links: <a href=\'http://fullmetal.boards.net/thread/51/based-gods-league-originally-oora\'>League Info</a> | </font><a href=\'https://pokemonshowdown.com/damagecalc/\'> Damage Calculator</a> | <a href=\'http://fullmetal.boards.net/\'>Forums</a> | <a href=\'http://fullmetal.boards.net/board/9/rate-team\'>Rate my teams</a></center><hr><center><font size=\'7\'></font></center><center>Server Time: ' + server_time + '<br><a href="http://www.basedgods.co.nr">Showdown Server!</a><center>Current Steamroller: ' + steam_name + '<center> Score: ' + steam_score + '</center></center><td><br><img src=\'pokemon:491\' align=right></td></tr></table> </b>')
 
         }
-         if (stepCounter % 900 === 0){
-				var users = ["±Flippy", "±Random Guy", "±Glorious Prostitute", "±Elderly Pedophile", "±Omar", "±Harry Pothead", "±Infamous Sex Addict", "±Friendly Drug Dealer", "±Attractive Carsalesman", "±Drunken Nana", "±Pregnant Suicidal Teen", "±Fleece Johnson", "±Wrong Caller ID", "±Ebola Stricken Cowboy", "±Below Average IQ Waitress"]
-             var random_name = users[Math.floor(Math.random() * users.length)];
-             var messages = ["I'll slap you so hard, even google won't be able to find you.", "It's your birthday? Well merry thanksgiving easter bunny.", "All my life I thought air was free, until I bought a bag of crisps.", "Hey I'll be back in 5 minutes, but if i'm not, just read this message again.", "Deja Poo, that feeling that you've heard this shit before.", "You call me fat!? Oh hell no, hold my cake.", "When I die, I want my last words to be: I left a million dollars under the...", "The worst thing about eating an entire block of cheese by yourself... is everything I just said.", "Roads? Where we're going we don't need roads.", "What does a nosey pepper do?  Get jalapeno business!", "What do you call a fake noodle? An impasta.", "I hate when people say age ain't nothing but a number, age is clearly a word.", "Shout out to all the bathroom models, make sure you charge your phone and windex your mirror before your next photoshoot.", "If you ain't got no money take yo broke ass home."]
-				var random_message = messages[Math.floor(Math.random() * messages.length)];
+        if (stepCounter % 900 === 0) {
+            var users = ["±Flippy", "±Random Guy", "±Glorious Prostitute", "±Elderly Pedophile", "±Omar", "±Harry Pothead", "±Infamous Sex Addict", "±Friendly Drug Dealer", "±Attractive Carsalesman", "±Drunken Nana", "±Pregnant Suicidal Teen", "±Fleece Johnson", "±Wrong Caller ID", "±Ebola Stricken Cowboy", "±Below Average IQ Waitress"]
+            var random_name = users[Math.floor(Math.random() * users.length)];
+            var messages = ["I'll slap you so hard, even google won't be able to find you.", "It's your birthday? Well merry thanksgiving easter bunny.", "All my life I thought air was free, until I bought a bag of crisps.", "Hey I'll be back in 5 minutes, but if i'm not, just read this message again.", "Deja Poo, that feeling that you've heard this shit before.", "You call me fat!? Oh hell no, hold my cake.", "When I die, I want my last words to be: I left a million dollars under the...", "The worst thing about eating an entire block of cheese by yourself... is everything I just said.", "Roads? Where we're going we don't need roads.", "What does a nosey pepper do?  Get jalapeno business!", "What do you call a fake noodle? An impasta.", "I hate when people say age ain't nothing but a number, age is clearly a word.", "Shout out to all the bathroom models, make sure you charge your phone and windex your mirror before your next photoshoot.", "If you ain't got no money take yo broke ass home."]
+            var random_message = messages[Math.floor(Math.random() * messages.length)];
             var waitress_message = "Would you like a table?"
             var me_name = "±Based Insider"
             var me_message = "No not at all, I came here to sit on the floor. Carpet for 5 please."
-            if(random_name == "±Omar"){
-               random_message = "I'm coming."
-             }
-             var col1 = +Math.floor(Math.random() * 9) + 1
-				var col2 = +Math.floor(Math.random() * 9) + 1
-				var col3 = +Math.floor(Math.random() * 9) + 1
-				var col4 = +Math.floor(Math.random() * 9) + 1
-				var col5 = +Math.floor(Math.random() * 9) + 1
-				var col6 = +Math.floor(Math.random() * 9) + 1
-            var random_colour = "#"+col1+""+col2+""+col3+""+col4+""+col5+""+col6;
-            if(random_name == "±Below Average IQ Waitress"){
-            	sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + random_name + ":</b> " + waitress_message +"</i></font>", 0);
-             sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + me_name + ":</b> " + me_message +"</i></font>", 0);
+            if (random_name == "±Omar") {
+                random_message = "I'm coming."
+            }
+            var col1 = +Math.floor(Math.random() * 9) + 1
+            var col2 = +Math.floor(Math.random() * 9) + 1
+            var col3 = +Math.floor(Math.random() * 9) + 1
+            var col4 = +Math.floor(Math.random() * 9) + 1
+            var col5 = +Math.floor(Math.random() * 9) + 1
+            var col6 = +Math.floor(Math.random() * 9) + 1
+            var random_colour = "#" + col1 + "" + col2 + "" + col3 + "" + col4 + "" + col5 + "" + col6;
+            if (random_name == "±Below Average IQ Waitress") {
+                sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + random_name + ":</b> " + waitress_message + "</i></font>", 0);
+                sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + me_name + ":</b> " + me_message + "</i></font>", 0);
 
             } else {
-				sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + random_name + ":</b> " + random_message +"</i></font>", 0);
-           }
-		  }
+                sys.sendHtmlAll("<i><font color=" + random_colour + "><timestamp/><b>" + random_name + ":</b> " + random_message + "</i></font>", 0);
+            }
+        }
         require.callPlugins("step");
     },
     warning: function warning(func, message, backtrace) {
@@ -293,7 +303,7 @@ var ampm = d.getHours() < 12 ? 'AM' : 'PM';
     afterNewMessage: function afterNewMessage(message) {
         require.callPlugins("afterNewMessage", message);
     },
-    beforeServerMessage: function (message) {
+    beforeServerMessage: function(message) {
         require.callPlugins("beforeServerMessage", message);
     },
     beforeChannelJoin: function beforeChannelJoin(src, channel) {
@@ -352,7 +362,7 @@ var ampm = d.getHours() < 12 ? 'AM' : 'PM';
     afterChatMessage: function afterChatMessage(src, message, chan) {
         require.callPlugins("afterChatMessage", src, message, chan);
     },
-    beforePlayerRegister: function (src) {
+    beforePlayerRegister: function(src) {
         Utils.watch.notify(Utils.nameIp(src) + " registered.");
     },
     battleConnectionLost: function() {
